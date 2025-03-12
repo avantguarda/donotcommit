f1a21b2c3bf297070d8842c1fcab816fc962abd8
ivansantiagojr <ivansantiago.junior@gmail.com>
2025-03-11 21:58:41 -0300

    feat: develop list gitignore templates endpoint

--- src/main.py
+++ src/main.py
@@ -1,4 +1,6 @@
-from fastapi import FastAPI
+from pathlib import Path
+
+from fastapi import FastAPI, Response
 
 app = FastAPI()
 
@@ -6,3 +8,23 @@ app = FastAPI()
 @app.get('/')
 async def read_root():
     return {'message': 'Hello, world!'}
+
+
+@app.get('/api/list')
+async def list_templates() -> Response:
+    """Lists all available gitignore templates by github.com/github/gitignore"""
+    project_root = Path(__file__).parent.parent
+    gitignore_folder = project_root / 'gitignore'
+
+    templates = gitignore_folder.glob('*.gitignore')
+    language_names = sorted([
+        Path(file).name.lower().removesuffix('.gitignore')
+        for file in templates
+    ])
+
+    formatted_names = ',\n'.join(
+        ','.join(language_names[i : i + 5])
+        for i in range(0, len(language_names), 5)
+    )
+
+    return Response(content=formatted_names, media_type='application/text')
